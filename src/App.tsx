6c18ec79017263e48bcb8c35deea87a40f213872
amitsafi45 <safiamit45@gmail.com>
2022-11-23 23:17:19 +0545

    test: check project runnable or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import {Counter} from '../src/counter'
 function App() {
   return (
     <div className="App">
+    <Counter/>
     </div>
   );
 }
---
+++ src/counter.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Counter ()  {
+  return (
+    <div>counter</div>
+  )
+}
